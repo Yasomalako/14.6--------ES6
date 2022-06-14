e9569244dfa7155bfd57c2c45fd3f1c36694dd38
Yasomalako <Yasomalako@gmail.com>
2022-06-14 19:50:25 +0300

    sort

---
+++ main.js
@@ -0,0 +1,13 @@
+//1/
+//כתבו פונקציה במקבלת מערך של סטרינגים וממינת אאותו על פי האלף בית
+let stringArray = ['sunday','monday','tusday','saterday'];
+stringArray.sort()
+console.log(stringArray);
+
+//2/
+//כתבו פונקציה המקבלת מערך של מספרים וממינת אותו מהקטן לגדול
+const numbers = [4,15,32,22]
+numbers.sort((b,a) => b-a)
+console.log(numbers)
+//3/
+//
